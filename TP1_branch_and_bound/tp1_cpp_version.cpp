0356f76a008b2fe14c2fe7174caa45ef4d1f68ac
Aimad <ia_chabounia@esi.dz>
2022-03-17 11:51:53 +0100

    final(maybe) version c++

--- TP1_branch_and_bound/tp1_cpp_version.cpp
+++ TP1_branch_and_bound/tp1_cpp_version.cpp
@@ -10,8 +10,9 @@ struct Node
   float profit;
   int32_t w;
   float eval;
+  int32_t index;
   shared_ptr<Node> parent;
-  Node(int32_t a, int32_t b, float c, int32_t d, float e, shared_ptr<Node> p): i(a), x(b), profit(c), w(d), eval(e), parent(p) {}
+  Node(int32_t a, int32_t b, float c, int32_t d, float e, shared_ptr<Node> p, int32_t index): i(a), x(b), profit(c), w(d), eval(e), parent(p), index(index) {}
 };
 
 void afficher_solution(shared_ptr<Node> solution)
@@ -20,61 +21,67 @@ void afficher_solution(shared_ptr<Node> solution)
   cout << "Max profit : " << node->profit << endl;
   while(node->i != 0 && node != nullptr)
   {
-    cout << "x" << node->i << " = " << node->x << endl;
+    cout << "x" << node->index << " = " << node->x << endl;
     node = node->parent;
   }
 }
 
-void branch_and_bound(const vector<pair<int32_t, int32_t>>& items, int32_t W)
+void branch_and_bound(const vector<vector<int32_t>>& items, int32_t W)
 {
   //solution récursive
   //W : capacité max
   stack<shared_ptr<Node>> st;
-  st.push(make_shared<Node>(0, 0, 0, W, INT_MAX, nullptr));
-  vector<shared_ptr<Node>> candidates;
+  st.push(make_shared<Node>(0, 0, 0, W, INT_MAX, nullptr, 0));
+  //vector<shared_ptr<Node>> candidates;
   int32_t n = items.size();
   float M = -oo; //borne inf
+  shared_ptr<Node> solution;
   while(!st.empty())
   {
     shared_ptr<Node> top = st.top();
     st.pop();
     if(top->i == n)
     {
-      candidates.push_back(top);
-      M = fmaxf(M, top->profit);
+      //candidates.push_back(top);
+      if(top->profit > M)
+      {
+        solution = top;
+        M = fmaxf(M, top->profit);
+      }
     }
     else if(top->i < n)
     {
       int32_t new_w = top->w;
       int32_t new_i = top->i + 1; //next i
+      int32_t new_index = items[top->i][2]+1;
       int32_t xi;
-      for(xi = 0; top->w-items[top->i].second*xi >= 0; xi++)
+      for(xi = 0; top->w - items[top->i][1]*xi >= 0; xi++)
       {
         int32_t new_x = xi;
-        float new_profit = top->profit + items[top->i].first*xi;
-        new_w = top->w-items[top->i].second*xi;
+        float new_profit = top->profit + items[top->i][0]*xi;
+        new_w = top->w - items[top->i][1]*xi;
         float new_eval = top->eval;
         shared_ptr<Node> new_parent = top;
-        if(new_profit < new_eval)
-        {
-          st.push(make_shared<Node>(new_i, new_x, new_profit, new_w, new_eval, new_parent));
-        }
-      }
-      if(new_w > 0)
-      {
-
-        if(new_i < n)
-        {
-          shared_ptr<Node> new_parent = top;
-          int32_t new_x = xi;
-          float new_profit = top->profit + items[top->i].first*xi;
-          float new_eval = fminf(top->eval, (items[new_i].first/items[new_i].second)*new_w);
-          if(new_eval > M) st.push(make_shared<Node>(new_i, new_x, new_profit, new_w, new_eval, new_parent));
-        }
+        //if(new_profit <= top->eval)
+        //{
+          if(new_i < n)
+          {
+            shared_ptr<Node> new_parent = top;
+            int32_t new_x = xi;
+            new_eval = fminf(top->eval, new_profit + (items[new_i][0]/items[new_i][1])*new_w);
+            if(new_eval >= M) st.push(make_shared<Node>(new_i, new_x, new_profit, new_w, new_eval, new_parent, new_index));
+          }
+          else
+          {
+            st.push(make_shared<Node>(new_i, new_x, new_profit, new_w, top->eval, new_parent, new_index));
+          }
+        //}
       }
     }
   }
+  /*
   shared_ptr<Node> solution = candidates.back();
+  cout << "number of possible solutions : " << candidates.size() << endl;
   for(auto& p : candidates)
   {
     if(p->profit > solution->profit)
@@ -82,10 +89,11 @@ void branch_and_bound(const vector<pair<int32_t, int32_t>>& items, int32_t W)
       solution = p;
     }
   }
+  */
   afficher_solution(solution);
-
 }
 
+/*
 void go(int32_t i, const vector<pair<int32_t, int32_t>>& items, float profit, int32_t W, float eval)
 {
   int32_t n = items.size();
@@ -99,10 +107,10 @@ void go(int32_t i, const vector<pair<int32_t, int32_t>>& items, float profit, in
   else
   {
     int32_t w = 0;
-    for(int32_t xi = 0; W-items[i].second*xi >= 0; xi++)
+    for(int32_t xi = 0; W-items[i][1]*xi >= 0; xi++)
     {
-      w = W-items[i].second*xi;
-      go(i+1, items, profit+items[i].first*xi, w, eval);
+      w = W-items[i][1]*xi;
+      go(i+1, items, profit+items[i][0]*xi, w, eval);
     }
     if (w>0)
     {
@@ -114,6 +122,7 @@ void go(int32_t i, const vector<pair<int32_t, int32_t>>& items, float profit, in
     }
   }
 }
+*/
 
 int main(void)
 {
@@ -122,17 +131,17 @@ int main(void)
   cin >> N;
   while(N--)
   {
-    vector<pair<int32_t, int32_t>> items;
+    vector<vector<int32_t>> items;
     int32_t n; cin >> n;
     int32_t W; cin >> W;
     vector<int32_t> a; for(int32_t i = 0; i < n; i++) { int32_t p; cin >> p; a.push_back(p);}
     vector<int32_t> b; for(int32_t i = 0; i < n; i++) { int32_t w; cin >> w; b.push_back(w);}
-    for(int32_t i = 0; i < n; i++) items.push_back(make_pair(a[i], b[i]));
-    auto comp = [&](const pair<int32_t, int32_t>& p1, const pair<int32_t, int32_t>& p2)
+    for(int32_t i = 0; i < n; i++) items.push_back(vector<int32_t>{a[i], b[i], i});
+    auto comp = [&](const vector<int32_t>& p1, const vector<int32_t>& p2)
     {
-      return (p1.first/p1.second) > (p2.first/p2.second);
+      return (float )((float )p1[0]/(float )p1[1]) > (float )((float )p2[0]/(float )p2[1]);
     };
-    sort(items.begin(), items.end());
+    sort(items.begin(), items.end(), comp);
     //cout << "this is recursive solution, it sucks ! (t3ayi)" << endl;
     //go(0, items, 0, W, INT_MAX);
     cout << "--------------------------------------------------------" << endl;
